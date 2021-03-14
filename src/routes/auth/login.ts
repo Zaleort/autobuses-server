dec571bc6204c60d1e3bb3b1d962b364448ed500
Alexandre Henrique <zaleortgames@outlook.com>
2021-03-14 02:58:37 +0100

    Usar Mongoose para el Login y Registro

--- src/routes/auth/login.ts
+++ src/routes/auth/login.ts
@@ -1,37 +1,42 @@
 import { Router } from 'express';
-import mongo from '../../mongo.js';
 import bcrypt from 'bcrypt';
 
 const router = Router();
 
 router.post('/auth/login', async (req, res, next) => {
-  const db = mongo.getDbCuentas();
-    const { usuario, contrasena } = req.body;
-    console.log('Intentando hacer login');
+  const usuariosModel = req.db?.usuarios;
 
-    try {
-      const user = await db.collection('usuarios').findOne({ usuario }, { projection: { contrasena: 0 } });
-      if (!user) {
-        console.log('El usuario no existe');
-        res.status(401).json({ message: 'Usuario o contraseña inválido' });
-        return;
-      }
+  if (!usuariosModel) {
+    res.status(500).json({ message: 'Error conectando con la base de datos' });
+    return;
+  }
 
-      const pass = contrasena;
-      const dbPass = user.contrasena;
+  const { usuario, contrasena } = req.body;
+  console.log('Intentando hacer login');
 
-      const compare = await bcrypt.compare(pass, dbPass);
-      if (compare) {
-        console.log('Login realizado con éxito');
-        res.status(200).json(user);
-        return;
-      }
-
-      console.log('La contraseña es inválida');
+  try {
+    const user = await usuariosModel.findOne({ usuario }).select({ contrasena: 0 });
+    if (!user) {
+      console.log('El usuario no existe');
       res.status(401).json({ message: 'Usuario o contraseña inválido' });
-    } catch (error) {
-      res.status(500).json({ error });
+      return;
     }
+
+    const pass = contrasena;
+    const dbPass = user.contrasena;
+
+    const compare = await bcrypt.compare(pass, dbPass);
+    if (compare) {
+      console.log('Login realizado con éxito');
+      res.status(200).json(user);
+      return;
+    }
+
+    console.log('La contraseña es inválida');
+    res.status(401).json({ message: 'Usuario o contraseña inválido' });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 });
 
 export default router;
--- src/routes/auth/register.ts
+++ src/routes/auth/register.ts
@@ -1,34 +1,38 @@
 import { Router } from 'express';
-import mongo from '../../mongo.js';
 import bcrypt from 'bcrypt';
 
 const router = Router();
 
 router.post('/auth/register', async (req, res, next) => {
-  const db = mongo.getDbCuentas();
-    const { usuario, contrasena } = req.body;
-    console.log(`Intentando registrar usuario ${usuario}`);
+  const usuariosModel = req.db?.usuarios;
 
-    try {
-      let user = await db.collection('usuarios').findOne({ usuario });
+  if (!usuariosModel) {
+    res.status(500).json({ message: 'Error conectando con la base de datos' });
+    return;
+  }
+  const { usuario, contrasena } = req.body;
+  console.log(`Intentando registrar usuario ${usuario}`);
 
-      if (user) {
-        console.log('El usuario ya existe');
-        res.status(500).json({ message: 'Usuario ya existe' });
-        return;
-      }
+  try {
+    let user = await usuariosModel.findOne({ usuario });
 
-      const hashPass = await bcrypt.hash(contrasena, 10);
-
-      user = await db.collection('usuarios').insertOne({ usuario, contrasena: hashPass });
-      usuario.contrasena = undefined;
-      console.log('Usuario creado correctamente');
-
-      res.status(200).json(usuario);
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ error });
+    if (user) {
+      console.log('El usuario ya existe');
+      res.status(500).json({ message: 'Usuario ya existe' });
+      return;
     }
+
+    const hashPass = await bcrypt.hash(contrasena, 10);
+
+    user = await usuariosModel.create({ usuario, contrasena: hashPass });
+    usuario.contrasena = undefined;
+    console.log('Usuario creado correctamente');
+
+    res.status(200).json(usuario);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error });
+  }
 });
 
 export default router;
