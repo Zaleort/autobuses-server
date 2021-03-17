b8fb43f08154e636c9fa47ec628eaec14ec63e90
Alexandre Henrique <zaleortgames@outlook.com>
2021-03-17 17:11:47 +0100

    Middleware para autenticar token y generarlo

---
+++ src/lib/accessToken.ts
@@ -0,0 +1,19 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+export function authenticateToken(req: Request, res: Response, next: NextFunction) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET as string, (err: any, user: any) => {
+    console.warn(err);
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
+
+export function generateToken(usuario: string) {
+  return jwt.sign(usuario, process.env.ACCESS_TOKEN_SECRET as string);
+}
